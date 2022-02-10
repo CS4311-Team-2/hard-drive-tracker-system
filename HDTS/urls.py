44831b85ac24d1e58d3ef20546016988fb9e2137
alibasimabed <63923487+alibasimabed@users.noreply.github.com>
2022-02-09 22:03:23 -0700

    ali-abed first app

--- HDTS/urls.py
+++ HDTS/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from hard_drive.views import drive_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hard_drive/', drive_view),
 ]
