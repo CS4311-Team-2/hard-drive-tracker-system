5c4f415da12a3390ff829db71c05084c119ee0f7
Eddie Garcia <edd1e234g@gmail.com>
2022-02-14 20:12:02 -0700

    Add dummy hard drive json & model

--- hard_drive/models.py
+++ hard_drive/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from .models import HardDrive
 
 # Create your models here.
+
+class HardDrive(models.Model):
+    id = models.TextField(primary_key=True)
+    #TODO Need to add foreign key to request. 
+
--- hard_drive/views.py
+++ hard_drive/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Dummy data of hard Drive
+hard_drive = {
+    "id":"",
+    "status":"On",
+    "serial_numbers":"",
+    "classification":"classified",
+    "image_version":"image_1",
+    "date_of_last_boot_test":123141241,#timestamp
+    "boot_test_status":"",
+    "connection_port":"",
+    "issue_date":19234352, #timestamp
+    "text_comment":"",
+    "request_id":""
+}
+
 
 # Create your views here.
 
