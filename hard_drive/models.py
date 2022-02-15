65177c9a1ff3b5a40e4a67e13dd0b13355740a72
Eddie Garcia <edd1e234g@gmail.com>
2022-02-14 21:07:01 -0700

    Sample code for hard drive model

--- hard_drive/models.py
+++ hard_drive/models.py
@@ -1,9 +1,23 @@
+from email.policy import default
 from django.db import models
-from .models import HardDrive
+from django.utils import timezone
+from unixtimestampfield.fields import UnixTimeStampField
+
 
 # Create your models here.
 
 class HardDrive(models.Model):
     id = models.TextField(primary_key=True)
+    # TODO: Make sure this field when the object is created. 
+    create_date = UnixTimeStampField(auto_now_add=True) 
+    manufacturer = models.TextField(blank=True, default='')
+    hard_drive_type = models.TextField(blank=True, default='')
+    connection_port = models.TextField(blank=True, default='')
+    hard_drive_size = models.TextField(blank=True, default='')
+    status = models.TextField(blank=True, default='')
+    serial_number = models.TextField(blank=True, default='')
+    classification = models.TextField(blank=True, default='')
+    image_verion_id = models.TextField(blank=True, default='')
+    boot_test_expiration = UnixTimeStampField(default=timezone.now)
+    boot_test_status = models.TextField(blank=True, default='')
     #TODO Need to add foreign key to request. 
-
--- hard_drive/views.py
+++ hard_drive/views.py
@@ -2,18 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Dummy data of hard Drive
+# All fields are taken from the SRS
 hard_drive = {
     "id":"",
-    "status":"On",
-    "serial_numbers":"",
+    "create_date": 123124321, #timestamp
+    "manufacturer": "", 
+    "model_number":"",
+    "hard_drive_type":"",
+    "connection_port":"",
+    "hard_drive_size":"", 
+    "status":"Assigned", #{Available, Assigned, End-of-life, etc}
+    "serial_number":"",
     "classification":"classified",
-    "image_version":"image_1",
-    "date_of_last_boot_test":123141241,#timestamp
+    "image_version_id": 1234,
+    "boot_test_expiration":123141241,#timestamp
     "boot_test_status":"",
+    "justification_for_hard_drive_status_change":"",
     "connection_port":"",
     "issue_date":19234352, #timestamp
-    "text_comment":"",
-    "request_id":""
+    "request_id":"" # Foreign Key to request
 }
 
 
