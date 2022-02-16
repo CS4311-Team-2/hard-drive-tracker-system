f3f3faf2a372e5d39ea67859dd4a2ce3ff5b0a93
chris chinolla <98130863+chinolla@users.noreply.github.com>
2022-02-16 16:03:52 -0700

    added new files and deleted the old ones (#14)

--- main/apps.py
+++ main/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class HardDriveConfig(AppConfig):
+class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'hard_drive'
+    name = 'main'
