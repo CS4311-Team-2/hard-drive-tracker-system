9dad360c6b91c70032744c4053cd3844ab584be3
Eddie Garcia <edd1e234g@gmail.com>
2022-02-21 10:34:04 -0700

    Fix up Hard Drive model attributes

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,17 @@
 from django import forms
 from .models import Listings
+from main.submodels.hard_drive import *
 
+# Template class
 class ListingForm(forms.ModelForm):
     class Meta:
         model = Listings
         fields = ['title', 'condition', 'product_type', 'sale_type', 'price', 'city', 'state', 'zipcode', 'contact_email', 'list_date']
+
+class HardDriveForm(forms.ModelForm):
+    class Meta:
+        model = HardDrive
+        fields = ['create_date', 'manufacturer', 'hard_drive_type', 'connection_port', 
+                'status', 'serial_number', 'classification', 'image_version_id', 
+                'boot_test_expiration', 'boot_test_status', 'justification_for_hard_drive_status_change', 
+                'connection_port', 'issue_date']
--- main/submodels/hard_drive.py
+++ main/submodels/hard_drive.py
@@ -7,21 +7,48 @@ from unixtimestampfield.fields import UnixTimeStampField
 #TODO: Need to create options for status, connection port, classification, 
 # and boot_test_status 
 class HardDrive(models.Model):
+
+    class Classification(models.TextChoices):
+        CLASSIFIED = "classified"
+        UNCLASSIFIED = "unclassified"
+
+    class BootTestStatus(models.TextChoices):
+        PASS = "pass"
+        FAILED = "failed"
+
     id = models.TextField(primary_key=True)
     # TODO: Make sure this field when the object is created. 
     create_date = UnixTimeStampField(auto_now_add=True) 
-    manufacturer = models.TextField(blank=True, default='')
-    hard_drive_type = models.TextField(blank=True, default='')
-    connection_port = models.TextField(blank=True, default='')
-    hard_drive_size = models.TextField(blank=True, default='')
-    status = models.TextField(blank=True, default='')
-    serial_number = models.TextField(blank=True, default='')
-    classification = models.TextField(blank=True, default='')
-    image_verion_id = models.TextField(blank=True, default='')
-    boot_test_expiration = UnixTimeStampField(default=timezone.now)
-    boot_test_status = models.TextField(blank=True, default='')
-    justification_for_hard_drive_status_change = models.TextField(blank=True, default='')
-    connection_port = models.TextField(blank=True, default='')
-    issue_date = UnixTimeStampField(default=timezone.now)
+    serial_number = models.CharField(max_length=100)
+    manufacturer = models.CharField(blank=True, max_length=100)
+    model_number = models.CharField(blank=True, max_length=100)
+    hard_drive_type = models.CharField(max_length=100)
+    connection_port = models.CharField(max_length=100)
+    hard_drive_size = models.CharField(max_length=100)
+    classification = models.CharField(max_length=50, choices=Classification.choices, 
+                                        default=Classification.UNCLASSIFIED)
+    # This field needs to be changed when the classification is changed. 
+    # TODO: This needs to give the option of a file or straight text. 
+    justification_for_classification_change = models.TextField()
+    # TODO: Need to limit this number at 4. 
+    image_verion_id = models.CharField(blank=True, default='')
+    boot_test_status = models.CharField(max_length=50, choices=BootTestStatus.choices, 
+                                            default=BootTestStatus.PASS)
+    boot_test_expiration = UnixTimeStampField()
 
-     #TODO Need to add foreign key to request. 
+    # The options to this field can be configured. 
+    status = models.CharField(max_length=100)
+    justification_for_hard_drive_status_change = models.TextField(blank=True)
+    issue_date = UnixTimeStampField(blank=True)
+    expected_hard_drive_return_date = UnixTimeStampField(blank=True)
+    justification_for_hard_drive_return_date_status_change = models.TextField(blank=True)
+    actual_return_date = UnixTimeStampField(blank=True)
+    modified_date = UnixTimeStampField(auto_add=True, auto_add_now=True)
+
+
+
+    class Meta:
+        verbose_name_plural = "Hard Drive"
+
+
+     #TODO Need to add foreign key to request.
