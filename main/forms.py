24332a9d363c3cd6deb34c25ccc75fdeaa188b21
Alex Avila <48698038+alexavila150@users.noreply.github.com>
2022-02-23 16:43:12 -0700

    Alex - Create Authentication and Login as set up to restric maintainer and requestor  (#35)
    * adding login page
    
    * login page
    
    * Login and Registration Page for new users

--- main/forms.py
+++ main/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from .models import Listings
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.models import User
 from main.models.hard_drive import HardDrive
 
 # Template class
@@ -8,6 +11,11 @@ class ListingForm(forms.ModelForm):
         model = Listings
         fields = ['title', 'condition', 'product_type', 'sale_type', 'price', 'city', 'state', 'zipcode', 'contact_email', 'list_date']
 
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields =['username', 'email', 'password1', 'password2']
+        
 class HardDriveForm(forms.ModelForm):
     class Meta:
         model = HardDrive
