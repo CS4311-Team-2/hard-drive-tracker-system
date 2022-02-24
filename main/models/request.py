a626a7eff740d3e15172bd4f7dc392927a4630f2
rilopez99316 <ilbroberto99316@gmail.com>
2022-02-23 23:04:39 -0700

    fixed minor bugs in fields
    Removed "models. " and added the missing keyword for getting request_status choices

--- main/models/request.py
+++ main/models/request.py
@@ -21,9 +21,9 @@ class Request(models.Model):
     
     request_reference_no = models.IntegerField()
     request_reference_no_year = UnixTimeStampField(auto_now_add=True)
-    request_status = models.CharField(max_length = 50, choices= Request_Status)
-    request_creation_date = models. UnixTimeStampField(auto_now_add=True)
-    request_last_modifed_date = models. UnixTimeStampField(auto_now=True, blank=True)
+    request_status = models.CharField(max_length = 50, choices= Request_Status.choices)
+    request_creation_date = UnixTimeStampField(auto_now_add=True)
+    request_last_modifed_date = UnixTimeStampField(auto_now=True, blank=True)
     need_drive_by_date = models.CharField(max_length = 50)
     comment = models.TextField(blank = True) 
     file_attachment = models.FileField()
