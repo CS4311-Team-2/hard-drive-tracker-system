06d77bc840b0e7026b5943ea9e69d700ca9def91
rilopez99316 <ilbroberto99316@gmail.com>
2022-02-23 22:36:09 -0700

    updated Request.py
    included fields request_reference_no and request_reference_no_year

--- main/models/request.py
+++ main/models/request.py
@@ -1,12 +1,8 @@
 from datetime import datetime
-from email.policy import default
 from django.db import models
+from unixtimestampfield.fields import UnixTimeStampField
 
 class Request(models.Model):
-    #class Request_Reference_No(models.Field):
-    #    year = datetime.year
-    #     referece_no = models.DecimalField(decimal_places = 0, max_digits= 4)
-    
 
     class Request_status(models.TextChoices):
         CREATED = "created"
@@ -23,11 +19,12 @@ class Request(models.Model):
         OVERDUE = "overdue"
     #Forecasted, confirmed shall be constrained by event status.?
     
-    #request_reference_no = models.CharField(Request_Reference_No())
+    request_reference_no = models.IntegerField()
+    request_reference_no_year = UnixTimeStampField(auto_now_add=True)
     request_status = models.CharField(max_length = 50, choice= Request_status)
-    request_creation_date = models.DateTimeField(auto_now_add=True)
-    request_last_modifed_date = models.DateTimeField(auto_now=True, blank=True)
-    need_drive_by_date = models.DateField() 
+    request_creation_date = models. UnixTimeStampField(auto_now_add=True)
+    request_last_modifed_date = models. UnixTimeStampField(auto_now=True, blank=True)
+    need_drive_by_date = models.CharField(max_length = 50)
     comment = models.TextField(blank = True)  
    
     class Meta:
