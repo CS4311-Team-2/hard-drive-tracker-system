e38d2c8cd675871f45d030fb7f1a63b27d1cd60f
chinolla <cjchinolla@miners.utep.edu>
2022-02-26 00:10:59 -0700

    changed the 'CREATED' thingy

--- main/models/request.py
+++ main/models/request.py
@@ -23,7 +23,7 @@ class Request(models.Model):
     
     request_reference_no = models.CharField(primary_key=True, max_length=50)
     request_reference_no_year = UnixTimeStampField(use_numeric=True, auto_now_add=True,default=0.0)
-    request_status = models.CharField(max_length = 50, choices= Request_Status.choices, default=CREATED)
+    request_status = models.CharField(max_length = 50, choices= Request_Status.choices, default=Request_Status.CREATED)
     request_creation_date = UnixTimeStampField(use_numeric=True, auto_now_add=True,default=0.0)
     request_last_modifed_date = UnixTimeStampField(use_numeric=True, auto_now=True,default=0.0)
     need_drive_by_date = models.CharField(max_length = 50)
