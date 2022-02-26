cda506558881ec9b4d99a1b7a2274f81424ac32c
rilopez99316 <ilbroberto99316@gmail.com>
2022-02-25 18:47:36 -0700

    linked event model to request model

--- main/models/event.py
+++ main/models/event.py
@@ -2,6 +2,8 @@ from datetime import datetime
 from django.db import models
 from unixtimestampfield.fields import UnixTimeStampField
 
+from main.models.request import Request
+
 class Event(models.Model):
     class Status(models.TextChoices):
         CONFIRMED = "confirmed"
@@ -16,6 +18,7 @@ class Event(models.Model):
     event_status = models.CharField(max_length=50, choices = Status.choices, default = Status.CONFIRMED)
     event_start_date = UnixTimeStampField(blank = True)
     event_end_date = UnixTimeStampField(blank = True)
+    event = models.ForeignKey(Request, on_delete=models.CASCADE, blank=True, null=True, related_name="event")
 
     class Meta:
         verbose_name_plural = "Event"
