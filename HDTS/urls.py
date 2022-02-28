c0c3c44bf72e04a75385d496ae72f7fed4125104
Alex <alexadrianavila@gmail.com>
2022-02-27 17:44:59 -0700

    requirements remane

--- HDTS/urls.py
+++ HDTS/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from pydoc import doc
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -29,3 +30,4 @@ urlpatterns = [
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
+urlpatterns=urlpatterns+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
