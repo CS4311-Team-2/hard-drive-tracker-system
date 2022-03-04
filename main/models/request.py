a246d7c75b25e517613f532671d005372e4ba1e8
Roberto Israel Lopez <60583807+rilopez99316@users.noreply.github.com>
2022-03-04 15:33:39 -0700

    Roberto hard drive request model (#50)
    * Created hard_drive_request.py file
    
    removed Class: HardDriveRequest from Class: Request. Created a new file called hard_drive_request.py for Class: HardDriveRequest.
    
    * added file: hard_drive_request.py and event into init...
    
    * linked event model to request model
    
    * fix import
    
    Co-authored-by: Alex <alexadrianavila@gmail.com>

--- main/models/request.py
+++ main/models/request.py
@@ -33,23 +33,3 @@ class Request(models.Model):
     class Meta:
         verbose_name_plural = "Request"
 
-
-class HardDriveRequest(models.Model):
-
-    class Classification(models.TextChoices):
-        CLASSIFIED = "classified"
-        UNCLASSIFIED = "unclassified"
-
-    # key to the hard drive. 
-    classification = models.CharField(max_length=50, choices=Classification.choices, 
-                                        default=Classification.UNCLASSIFIED)
-    amount_required = models.IntegerField(default=1) 
-    connection_port = models.CharField(max_length=50, default='SATA')
-    hard_drive_size = models.CharField(max_length=50,blank=True)
-    hard_drive_type = models.CharField(max_length=50,blank=True)
-    comment = models.TextField(blank=True)
-    request = models.ForeignKey(Request, 
-                on_delete=models.CASCADE, blank=True, null=True, related_name="hard_drive_requests")
-
-    class Meta:
-        verbose_name_plural = "Hard Drive Request"
