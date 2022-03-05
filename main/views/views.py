ec03e2b9ec1f050acab4b850e8fcf43489b3d398
rilopez99316 <ilbroberto99316@gmail.com>
2022-03-04 21:51:47 -0700

    linked database to form (add new hard drive)
    Fixed wrongly named attribute and added missing "missing attribute"
    
    Changed the naming of the "id"s to its corresponding attribute.
    
    created file linking form and database

--- main/views/views.py
+++ main/views/views.py
@@ -75,3 +75,29 @@ def loginPage(request):
 def logoutUser(request):
     logout(request)
     return redirect('main:login')
+
+def maintainer_adds_hard_drive():
+    hardDrive = HardDrive()
+    
+    if add_hard_drive.method == 'POST':
+        hardDrive.create_date = add_hard_drive.POST.get('creation_date')
+        hardDrive.serial_number = add_hard_drive.POST.get('serial_No')
+        hardDrive.manufacturer = add_hard_drive.POST.get('manufacturer')
+        hardDrive.model_number = add_hard_drive.POST.get('model_NO')
+        hardDrive.hard_drive_type = add_hard_drive.POST.get('hard_drive_type')
+        hardDrive.connection_port = add_hard_drive.POST.get('connection_por')
+        #hardDrive.hard_drive_size = add_hard_drive.POST.get('') not available yet in the interface
+        #hardDrive.Classification = add_hard_drive.POST.get('') How do you get the value for a dropdown?
+        hardDrive.justification_for_classification_change = add_hard_drive.POST.get('justification_for_classification_change')
+        hardDrive.image = add_hard_drive.POST.get('hardDrive_image')
+        hardDrive.image_version_id = add_hard_drive.POST.get('image_version_id')
+        hardDrive.boot_test_status = add_hard_drive.POST.get('boot_test_status')
+        hardDrive.boot_test_expiration = add_hard_drive.POST.get('boot_test_expiration_date')
+       
+        #hardDrive.status = add_hard_drive.POST.get('status') How do you get the value for a dropdown?
+        hardDrive.justification_for_hard_drive_status_change = add_hard_drive.POST.get('justification_for_hard_drive_status_change')
+        hardDrive.issue_date = add_hard_drive.POST.get('issue_date')
+        hardDrive.expected_hard_drive_return_date = add_hard_drive.POST.get('expected_hard_drive_return_date')
+        hardDrove.justification_for_hard_drive_return_date = add_hard_drive.POST.get('justification_for_hard_drive_return_date')
+        hardDrive.request = add_hard_drive.POST.get('request_reference')
+        
