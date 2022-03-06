aba878bb00d99a7a92b990f0153fe9af2c7e0eb3
7DavidMedina7 <60455398+7DavidMedina7@users.noreply.github.com>
2022-03-06 00:00:42 -0700

    David - Added event and request dummy data (#64)
    * Hard drive inventory interface for maintainer
    
    * Added maintainer view all hard drives for maintainer
    
    * Create maintainer_view_all_requests.html
    
    david - Created the first draft of the view all requests for the maintainer.
    
    Search Content Area - Date Attribute:
    Needs the date picker.
    
    * Maintainer View All Requests
    
    Fixed date picker
    
    * Added event and request dummy data
    
    * Removed spaces from the CSV files.
    
    * 1) Removed ID attribute from request.csv 2) Coverted the dates in both request.csv and event.csv to Unix timestamp format.
    
    * Removed the id attributes from the request model in updatemodels.py.
    
    * Changed attribute lenght_of_reporting_cycle to length_of_reporting_cycles in event.csv
    
    * Changed the length_of_reporting_cycles to Unix time
    
    * register Event to admin page
    
    Co-authored-by: Alex <alexadrianavila@gmail.com>

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Listings, HardDrive, Request, HardDriveRequest
+from .models import Listings, HardDrive, Request, HardDriveRequest, Event
 
 
 admin.site.register(Listings)
 admin.site.register(HardDrive)
 admin.site.register(Request)
 admin.site.register(HardDriveRequest)
+admin.site.register(Event)
