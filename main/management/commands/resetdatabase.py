d7bab889fefe37e93d5d51d2a300670a8c37af24
Alex <alexadrianavila@gmail.com>
2022-03-09 14:44:45 -0700

    added reset command

---
+++ main/management/commands/resetdatabase.py
@@ -0,0 +1,32 @@
+from subprocess import call
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+import os
+
+BaseCommand
+
+MIGRATIONS = 'main\migrations'
+
+class Command(BaseCommand):
+    help = 'import booms'
+
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        if os.path.exists("db.sqlite3"):
+            os.remove("db.sqlite3")
+        else:
+            print("The file does not exist")
+
+        for filename in os.listdir(MIGRATIONS):
+            f = os.path.join(MIGRATIONS, filename)
+
+            if os.path.isfile(f) and not f == 'main\migrations\__init__.py':
+                os.remove(f)
+
+        call_command('makemigrations')
+        call_command('migrate')
+        call_command('updatemodels')
+
+        
