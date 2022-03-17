4a8a156621884f6aa7ee0652579712303f95cabd
Eduardo Garcia <47912409+Edd1e234@users.noreply.github.com>
2022-03-16 21:17:24 -0600

    Eddie dynamically generate dropdown (#72)
    * Commit not working
    
    * Dropdown functionality works in a hacky way... only frontend

--- main/views/views.py
+++ main/views/views.py
@@ -6,17 +6,23 @@ from main.views import maintainer, requestor
 from ..models import HardDrive
 from ..models import Request
 from ..forms import CreateUserForm
+from django.http import Http404
 
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User, Group
 from django.http import Http404
 
+MAINTAINER = "Maintainer"
+
+def add_drive(request):
+    return render(request, 'maintainer/add_hard_drive.html')
 
 # These functions relate to the account/*.html and main/index.html. These functions relate 
 #   to logining in and creating account.  
-    
+
 @login_required(login_url='main:login')
 def index(request):
     if request.user.groups.filter(name='Maintainer').exists() | request.user.is_staff:
@@ -49,7 +55,6 @@ def make_request(request):
 
 def registerPage(request):
     form = CreateUserForm()
-
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid():
@@ -62,22 +67,38 @@ def registerPage(request):
     return render(request, 'accounts/register.html', context)
     
 def loginPage(request):
+    context = {
+        "show_menu" : False
+        }
     if request.user.is_authenticated:
-        return redirect('main:index')
-
+        print("User authorized!")
+        user = User.objects.get(username=request.user.username)
+        if user.groups.filter(name=Group(name=MAINTAINER)):
+            messages.info(request, 'Welcome Maintainer!')
+            context = {
+                "show_menu" : True
+            }
+        else:
+            return redirect('main:index')
 
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
+        auth = authenticate(request, username=username, password=password)
 
-        if user is not None:
-            login(request, user)
-            return redirect('main:index')
+        if auth is not None:
+            print("User credentials is correct")
+            login(request, auth)
+            user = User.objects.get(username=username)
+            print(user)
+            if user.groups.filter(name=Group(name=MAINTAINER)):
+                print("User is made it here")
+                return redirect('main:login')
+            else:
+                return redirect('main:index')
         else:
             messages.info(request, 'Username or password is incorrect')
 
-    context = {}
     return render(request, 'accounts/login.html', context)
 
 def logoutUser(request):
