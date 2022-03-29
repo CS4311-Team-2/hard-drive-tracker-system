2ad61a68c75176af3140fbf0dd515c2fa9eca0d8
alibasimabed <63923487+alibasimabed@users.noreply.github.com>
2022-03-29 16:03:43 -0600

    final

--- main/models/log.py
+++ main/models/log.py
@@ -9,6 +9,5 @@ from django.conf import settings
 class Log(models.Model):
     time_stamp = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1, on_delete=models.CASCADE)
-    action_preformed = models.CharField(max_length = 500)
-
+    action_performed = models.CharField(max_length = 500)
 
