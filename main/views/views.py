3473b6009901ec603d4ec8eb6a20693efea16c0c
rilopez99316 <ilbroberto99316@gmail.com>
2022-03-30 21:00:02 -0600

    Filter package added for filter feature

--- main/views/views.py
+++ main/views/views.py
@@ -14,6 +14,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User, Group
 from django.http import Http404
+from ..filters import OrderFilter
 
 MAINTAINER = "Maintainer"
 
@@ -118,7 +119,7 @@ def view_all_harddrives(request):
     if request.user.groups.filter(name='Maintainer').exists() | request.user.is_staff:
         return maintainer.view_all_harddrives(request)
         
-    
+    myFilter = OrderFilter()
     return redirect('main:index')
 
 
