2ea5b2dab544f7274310dbc8fa60dc839788ad6c
rilopez99316 <ilbroberto99316@gmail.com>
2022-04-01 14:18:41 -0600

    Updated Hard Drive Filter

--- main/filters.py
+++ main/filters.py
@@ -1,8 +1,10 @@
 import django_filters
+from django_filters import DateFilter
 
 from .models import HardDrive
 
 class HardDriveFilter(django_filters.FilterSet):
+    creation_date = DateFilter(field_name="create_date", lookup_expr="gte", label="Creation Date")
     class Meta:
         model = HardDrive
-        fields = '__all__'
+        fields = ['__all__', 'creation_date']
