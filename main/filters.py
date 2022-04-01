864d06d77054179aaba7c63359a39658d3bad072
rilopez99316 <ilbroberto99316@gmail.com>
2022-04-01 15:43:36 -0600

    Updated filter.py
    Forgot to fix error

--- main/filters.py
+++ main/filters.py
@@ -4,7 +4,7 @@ from django_filters import DateFilter
 from .models import HardDrive
 
 class HardDriveFilter(django_filters.FilterSet):
-    creation_date = DateFilter(field_name="create_date", lookup_expr="gte", label="Creation Date")
+    
     class Meta:
         model = HardDrive
-        fields = ['__all__', 'creation_date']
+        fields = '__all__'
