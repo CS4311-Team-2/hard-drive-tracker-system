3766bcdac129ce54c9eabcde439c2c2dc5bab1c6
Eddie Garcia <edd1e234g@gmail.com>
2022-04-08 09:27:29 -0600

    Add comments

--- main/tests.py
+++ main/tests.py
@@ -12,9 +12,9 @@ USERNAME = 'username'
 PASSWORD_STR = 'password'
 
 class HDTRTests(TestCase):
-
     @classmethod
     def setUpClass(self):
+        # Popluate the database in the test environment. 
         super().setUpClass()
         # Create Database
         Command.handle(self)
@@ -22,7 +22,7 @@ class HDTRTests(TestCase):
 
 class UpdateModelTests(HDTRTests):
     def test_deliquent_hard_drives(self):
-        # The database should have only 4 overdue hard drives.
+        # Tests that the database only has 3 overdue hard drives.
         requests = Request.objects.filter(request_status=Request.Request_Status.OVERDUE)
         hard_drives = HardDrive.objects.none()
         for r in requests:
@@ -33,29 +33,41 @@ class UpdateModelTests(HDTRTests):
 class URLTests(HDTRTests):
 
     def test_login_page_maintainer(self):
+        # Tests that the Maintainer logins and succesfully redirects. 
         c = Client()
         response = c.post('/login/', {USERNAME:MAINTAINER_USERNAME, PASSWORD_STR:PASSWORD})
         self.assertEqual(response.status_code, REDIRECT)
 
     def test_login_page_requestor(self):
+        # Tests that the Requestor logins and succesfully redirects. 
         c = Client()
         response = c.post('/login/', {USERNAME: REQUESTOR_USERNAME, 
                                         PASSWORD_STR:PASSWORD})
         self.assertEqual(response.status_code, REDIRECT)
 
     def test_login_page_admin(self):
+        # Tests that the Admin logins and succesfully redirects. 
         c = Client()
         response = c.post('/login/', {USERNAME: ADMIN_USERNAME, 
                                         PASSWORD_STR:PASSWORD})
         self.assertEqual(response.status_code, REDIRECT)
+
+    def test_login_page(self):
+        # Tests that incorrect login info does not redirect.
+        c = Client()
+        response = c.post('/login/', {USERNAME: '', 
+                                        PASSWORD_STR:''})
+        self.assertEqual(response.status_code, 200)
     
     def test_view_request_authenticated(self):
+        # Tests that the view request pages renders if user is authenticated.
         c = Client()
         c.login(username=MAINTAINER_USERNAME, password=PASSWORD)
         response = c.post('/view_request/')
         self.assertEqual(response.status_code, 200)
 
     def test_view_request_non_authenticated(self):
+        # Tests that the view request pages redirects if user is not authenicated.
         c = Client()
         response = c.post('/view_request/')
         self.assertEqual(response.status_code, REDIRECT)
