4327af28be1e5c9e44a97590a1a585c2d1751f00
Eddie Garcia <edd1e234g@gmail.com>
2022-04-13 12:33:19 -0600

    Merge

--- main/templatetags/has_group.py
+++ main/templatetags/has_group.py
@@ -9,6 +9,7 @@ def has_group(user, group_name):
     print("Has=Group")
 
     if user.groups.filter(name="Maintainer").exists():
+        # Need to manually check by role and mock_group
         if group_name == "Requestor" and user.mock_group_is == UserProfile.MockGroupIs.REQUESTOR:
             return True
         return group_name=="Maintainer" and user.mock_group_is == UserProfile.MockGroupIs.MAINTAINER
