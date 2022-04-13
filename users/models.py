fea0776cdaad2c59d5e7c190021e82e5c5652679
Eduardo Garcia <47912409+Edd1e234@users.noreply.github.com>
2022-04-13 13:15:18 -0600

    Eddie setup different views (#136)
    * A maintainer can view a requestor home
    
    * Semi Functional
    
    * Merge

--- users/models.py
+++ users/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class UserProfile(AbstractUser):
+
+    class MockGroupIs(models.TextChoices):
+        MAINTAINER = 'Maintainer'
+        REQUESTOR = 'Requestor'
+
     class Status(models.TextChoices):
         PENDING = 'Pending'
         ACTIVE = 'Active'
@@ -12,3 +17,6 @@ class UserProfile(AbstractUser):
     branch_chief_email = models.EmailField()
     status = models.CharField(max_length = 20, choices= Status.choices, default=Status.PENDING)
     last_modified_date = models.DateField(auto_now=True)
+
+    # Used only for a maintainer
+    mock_group_is = models.CharField(max_length=20, choices=MockGroupIs.choices, default=MockGroupIs.MAINTAINER)
