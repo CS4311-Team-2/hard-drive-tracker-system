882ee5d0df3843c9804d1d25868a56ac7e3c6272
Eduardo Garcia <47912409+Edd1e234@users.noreply.github.com>
2022-04-13 21:30:55 -0600

    Eddie requestor inventory view (#137)
    * A maintainer can view a requestor home
    
    * Semi Functional
    
    * Merge
    
    * Requestor can now see all their hard drives and view each hard drive
    
    * Maintainer mocking as a requestor is functional

--- users/models.py
+++ users/models.py
@@ -20,3 +20,4 @@ class UserProfile(AbstractUser):
 
     # Used only for a maintainer
     mock_group_is = models.CharField(max_length=20, choices=MockGroupIs.choices, default=MockGroupIs.MAINTAINER)
+
