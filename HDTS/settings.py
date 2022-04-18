6cb52e3a319ae6233b6f8bd319e85ae023cd9d0d
Alex <alexadrianavila@gmail.com>
2022-04-17 23:44:30 -0600

    esteban

--- HDTS/settings.py
+++ HDTS/settings.py
@@ -130,6 +130,10 @@ STATIC_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
