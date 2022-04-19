a68c38cd47cf132eb090fc05b9c2e6792d182180
Alex <alexadrianavila@gmail.com>
2022-04-19 14:46:06 -0600

    fix log filter user

--- main/filters.py
+++ main/filters.py
@@ -88,7 +88,6 @@ class UserProfile(django_filters.FilterSet):
 
 class LogFilter(django_filters.FilterSet):
     time_stamp = django_filters.DateFilter(field_name="time_stamp", lookup_expr="gte", label="Time Stamp From", widget=DateInput(attrs={'type': 'date'}))
-    user = django_filters.CharFilter(field_name="user", lookup_expr="icontains", label="Username")
     action_performed = django_filters.CharFilter(field_name="action_performed", lookup_expr="icontains", label="Action Performed")
     keyword = django_filters.CharFilter(method='search_all_log_fields',label="Search")
 
